a10424225df870862f39a63a4daa53c2ff76ba7f
Emil Rohlén <emil.rohlen@live.se>
2026-02-01 09:05:30 +0100

    expanded on entity base class

--- Assets/Scripts/Entities/Entity.cs
+++ Assets/Scripts/Entities/Entity.cs
@@ -1,27 +1,53 @@
 using Entities.Stats;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Entities
 {
     public abstract class Entity : MonoBehaviour
     {
-        [SerializeField] protected EntityBaseStats _stats;
+        [SerializeField] protected EntityBaseStats _entityBaseStats;
 
+        public UnityEvent<Entity> OnDeath;
+        
         protected int _maxHealth;
         protected int _currentHealth;
         
         protected float _moveSpeed;
         
         protected int _damage;
+        
+        private bool _isDead;
 
         private void Start()
         {
-            
+            StatsUpdated();
         }
 
         private void StatsUpdated()
         {
+            _maxHealth = _entityBaseStats.MaxHealth;
+            _moveSpeed = _entityBaseStats.MoveSpeed;
+            _damage = _entityBaseStats.Damage;
             
+            // TODO: Apply additional stats
+        }
+
+        public void TakeDamage(int amount)
+        {
+            _currentHealth -= amount;
+            
+            if (_currentHealth <= 0)
+                Die();
+        }
+        
+        protected void Die()
+        {
+            if (_isDead)
+                return;
+            
+            _isDead = true;
+            OnDeath?.Invoke(this);
         }
     }
 }
