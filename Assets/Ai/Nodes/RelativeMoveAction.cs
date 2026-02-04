eb4ffffe48acf478384e7423ada5ad5ca23bc933
b24oliwa <b24oliwa@student.his.se>
2026-02-04 16:02:44 +0100

    behaviour nodes

--- Assets/Ai/Nodes/RelativeMoveAction.cs
+++ Assets/Ai/Nodes/RelativeMoveAction.cs
@@ -1,49 +1,52 @@
 using System;
-using Unity.Behavior;
 using UnityEngine;
-using Action = Unity.Behavior.Action;
 using Unity.Properties;
 using UnityEngine.AI;
 
-[Serializable, GeneratePropertyBag]
-[NodeDescription(name: "Relative Move", story: "[Agent] moves relative to [Target]", category: "Action", id: "e6034d269de7d693e4e6c36aee4e104f")]
-public partial class RelativeMoveAction : Action
+namespace Unity.Behavior
 {
-    [SerializeReference] public BlackboardVariable<GameObject> Agent;
-    [SerializeReference] public BlackboardVariable<GameObject> Target;
-    [SerializeReference] public BlackboardVariable<float> StepDistance = new BlackboardVariable<float>(2.0f);
-    [SerializeReference] public BlackboardVariable<float> DirectionAngle = new BlackboardVariable<float>(0f);
-    private NavMeshAgent m_NavMeshAgent;
-    
-    protected override Status OnStart()
+    [Serializable, GeneratePropertyBag]
+    [NodeDescription(name: "Relative Move", story: "[Agent] moves relative to [Target]", category: "Action/Navigation",
+        id: "e6034d269de7d693e4e6c36aee4e104f")]
+    public partial class RelativeMoveAction : Action
     {
-        m_NavMeshAgent = Agent.Value.GetComponentInChildren<NavMeshAgent>();
-        if (!m_NavMeshAgent) return Status.Failure;
-        return Status.Running;
-    }
+        [SerializeReference] public BlackboardVariable<GameObject> Agent;
+        [SerializeReference] public BlackboardVariable<GameObject> Target;
+        [SerializeReference] public BlackboardVariable<float> StepDistance = new BlackboardVariable<float>(2.0f);
+        [SerializeReference] public BlackboardVariable<float> DirectionAngle = new BlackboardVariable<float>(0f);
+        private NavMeshAgent _navMeshAgent;
 
-    private bool wait;
-    private int waiter;
-    protected override Status OnUpdate()
-    {
-        
-        if (!wait)
+        protected override Status OnStart()
         {
-            var direction = Target.Value.transform.position - Agent.Value.transform.position;
-            direction = Quaternion.Euler(0,DirectionAngle,0) * direction;
-            m_NavMeshAgent.SetDestination(Agent.Value.transform.position + (direction.normalized*StepDistance.Value));
-            wait = true;
+            _navMeshAgent = Agent.Value.GetComponentInChildren<NavMeshAgent>();
+            return !_navMeshAgent ? Status.Failure : Status.Running;
         }
-        if (wait && m_NavMeshAgent.remainingDistance <= m_NavMeshAgent.stoppingDistance)
-        {
-            wait = false;
-        }
-        return Status.Running;
-    }
 
-    protected override void OnEnd()
-    {
-        m_NavMeshAgent = null;
+        private bool wait;
+        private int waiter;
+
+        protected override Status OnUpdate()
+        {
+            if (!wait)
+            {
+                var direction = Target.Value.transform.position - Agent.Value.transform.position;
+                direction = Quaternion.Euler(0, DirectionAngle, 0) * direction;
+                _navMeshAgent.SetDestination(Agent.Value.transform.position +
+                                             (direction.normalized * StepDistance.Value));
+                wait = true;
+            }
+
+            if (wait && _navMeshAgent.remainingDistance <= _navMeshAgent.stoppingDistance)
+            {
+                wait = false;
+            }
+
+            return Status.Running;
+        }
+
+        protected override void OnEnd()
+        {
+            _navMeshAgent = null;
+        }
     }
 }
-
