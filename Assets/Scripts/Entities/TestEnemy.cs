e4b1778b7e37ba67bc2540057505f36c2852dc55
b24oliwa <b24oliwa@student.his.se>
2026-02-16 13:59:22 +0100

    Enemies attacking and stuff

--- Assets/Scripts/Entities/TestEnemy.cs
+++ Assets/Scripts/Entities/TestEnemy.cs
@@ -1,4 +1,5 @@
 using System;
+using Stats;
 using Unity.Behavior;
 using UnityEngine;
 using UnityEngine.AI;
@@ -14,11 +15,16 @@ public class TestEnemy : Entity
     private BehaviorGraphAgent AiAgent;
     private NavMeshAgent navAgent;
 
+    [SerializeField] private Blackboard blackboard;
     [SerializeField] private BehaviorGraph behaviorGraph;
     public float attackCooldown;
 
-    private void Awake()
+    public GameObject attackPrefab;
+    public AttackStats attackStats;
+
+    protected override void Awake()
     {
+        base.Awake();
         InitializeBaseStats();
         rb = GetComponent<Rigidbody>();
         AiAgent = GetComponent<BehaviorGraphAgent>();
@@ -30,9 +36,20 @@ public class TestEnemy : Entity
         AiAgent.Start();
         AiAgent.SetVariableValue("Target", _player);
         AiAgent.SetVariableValue("AttackDelay", attackCooldown);
+        AiAgent.SetVariableValue("Animator", GetComponent<Animator>());
         navAgent.speed = _moveSpeed;
+        attackStats = new AttackStats(attackPrefab, 5, 0, 0, 1);
     }
-    
+
+    public void Attack()
+    {
+        Abilities.Attacks.Attack.Create(this,transform.position,transform.rotation,attackStats);
+    }
+
+    public void AttackFinished()
+    {
+        AiAgent.SetVariableValue("Attacking", false);
+    }
 
     private void Update()
     {
@@ -47,7 +64,8 @@ public class TestEnemy : Entity
         AiAgent.End();
         navAgent.enabled = false;
         rb.constraints = RigidbodyConstraints.None;
-        Destroy(this);
+        tag = "Untagged";
+        this.enabled = false;
     }
 
 
