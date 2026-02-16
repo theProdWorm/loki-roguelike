a9b9c01b836539573c820dcde19ad37c8403d544
Emil Rohlen <emil.rohlen@live.se>
2026-02-16 21:36:56 +0100

    started adding animator behavior scripts for animation lock

--- Assets/Scripts/Entities/Player/Player_Dash.cs
+++ Assets/Scripts/Entities/Player/Player_Dash.cs
@@ -1,34 +1,29 @@
 using UnityEngine;
 
-public class Player_Dash : StateMachineBehaviour
+namespace Entities.Player
 {
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    public class Player_Dash : StateMachineBehaviour
+    {
+        private Player _player;
+    
+        public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            _player = animator.GetComponentInParent<Player>();
+            
+            _player.LoseControl();
+            _player.SetDashing(true);
+        }
 
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+        // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+        //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        //{
+        //    
+        //}
 
-    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
+        public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+        {
+            _player.GainControl();
+            _player.SetDashing(false);
+        }
+    }
 }
