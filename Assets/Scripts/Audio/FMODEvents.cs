ad2004a562365e91b40e3320062f1298be738a87
Anton <c24antka@student.his.se>
2026-02-18 12:45:16 +0100

    Small changes in Audio Scripts
    Small changes in Audio Scripts

--- Assets/Scripts/Audio/FMODEvents.cs
+++ Assets/Scripts/Audio/FMODEvents.cs
@@ -1,12 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using FMODUnity;
 
 
 public class FMODEvents : MonoBehaviour
 {
-    [field: Header("RoundStart SFX")]
+    [field: Header("Player SFX")]
     
-    [field: SerializeField] public EventReference roundStarted { get; private set; }
+    [field: SerializeField] public EventReference meleeAttack { get; private set; }
     public static FMODEvents instance { get; private set; }
 
     private void Awake()
