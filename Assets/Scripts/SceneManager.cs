7cd922c742b7dba852e6e83bf44b9bad43529290
Emil Rohlen <emil.rohlen@live.se>
2026-02-27 11:08:14 +0100

    async functions for scene manager

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -3,10 +3,15 @@ using UnityEngine.SceneManagement;
 
 public class SceneManager : MonoBehaviour
 {
-    public void LoadScene(int sceneIndex)
-    {
+    public void LoadScene(int sceneIndex) => 
         UnityEngine.SceneManagement.SceneManager.LoadScene(sceneIndex);
-    }
+    public void LoadSceneAdditive(int sceneIndex) => 
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneIndex, LoadSceneMode.Additive);
+    public void LoadSceneAdditiveAsync(int sceneIndex) =>
+        UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Additive);
+    
+    public void UnloadSceneAsync(int sceneIndex) =>
+        UnityEngine.SceneManagement.SceneManager.UnloadSceneAsync(sceneIndex);
     
     public void QuitGame() => Application.Quit();
 }
