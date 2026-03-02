33eed754bae432b20e8b438699919df03ce355aa
L11X <linus_eriksson@telia.com>
2026-03-02 22:53:11 +0100

    Rudamentary statue system, unsure how well it works

--- Assets/Scripts/Encounters/SpawnPoint.cs
+++ Assets/Scripts/Encounters/SpawnPoint.cs
@@ -1,24 +1,55 @@
 ﻿using UnityEngine;
 using Entities;
+using Helpers;
+using UnityEngine.AI;
 
 public class SpawnPoint : MonoBehaviour
 {
     [SerializeField, Tooltip("The sort of enemy that spawns here")]
     private EncounterManager.EnemyTypes _enemyType;
 
+    [Header("Draugr")]
     [SerializeField]
-    private GameObject _draugrPrefab;
+    private bool _sittingStatue = false;
+    public bool hasSpawned = false;
+
+    [SerializeField, Tooltip("DEV TOOL. Designers don't touch")]
+    private Component[] _componentsToTurnOffAndOn; //TODO: Fix this ugly ass solution
+
+    Animator animator;
+
+    private float _animatorSpeed; //Holder for the animation speed of current clip
+
+    [Header("UGLY SOLUTION, WILL BE FIXED")]
+    [SerializeField]
+    private Enemy _enemyScript;
+    [SerializeField]
+    private Behaviour _behaviourAgentComponent;
+    [SerializeField]
+    private NavMeshAgent _navMeshAgentComponent;
+    [SerializeField]
+    private AnimationEventListener _animationEventListenerScript;
+
+    [Header("Bird on Bird")]
     [SerializeField]
     private GameObject _bbPrefab;
+
+    [Header("Wolf")]
     [SerializeField]
     private GameObject _wolfPrefab;
+
+    private void Start()
+    {
+        if (!hasSpawned)
+            PrepareDraugrStatue();
+    }
 
     public Entity Spawn()
     {
         switch (_enemyType)
         {
-            case EncounterManager.EnemyTypes.Draugr:
-                return SpawnDraugr();
+            //case EncounterManager.EnemyTypes.Draugr:
+            //    return SpawnDraugr();
             case EncounterManager.EnemyTypes.BirdOnBird:
                 return SpawnBirdOnBird();
             case EncounterManager.EnemyTypes.Wolf:
@@ -30,19 +61,43 @@ public class SpawnPoint : MonoBehaviour
         return null;
     }
 
-    bool _hasSpawned = false; // Temp variable
+    private void PrepareDraugrStatue()
+    {
+        animator = GetComponent<Animator>();
+
+        animator.SetBool("HasSpawned", false);
+        animator.SetBool("Sitting", _sittingStatue);
+
+        #region Ugly solution
+        _enemyScript.enabled = false;
+        _behaviourAgentComponent.enabled = false;
+        _navMeshAgentComponent.enabled = false;
+        _animationEventListenerScript.enabled = false;
+        #endregion
+
+        _animatorSpeed = animator.speed;
+        animator.speed = 0f;
+
+        //TODO: Pick a random frame in the animation
+    }
+
     private Entity SpawnDraugr()
     {
-        if (_hasSpawned) return null;
-        _hasSpawned = true;
+        //Safety check
+        if (hasSpawned) return null;
+        hasSpawned = true;
 
         GetComponent<MeshRenderer>().enabled = false;
         GetComponent<Collider>().enabled = false;
 
-        //TODO: OBJECTPOOL
-        GameObject _draugr = Instantiate(_draugrPrefab, transform.position, Quaternion.identity);
+        Entity _entity = GetComponent<Entity>();
 
-        Entity _entity = _draugr.GetComponent<Entity>();
+        #region Ugly solution
+        _enemyScript.enabled = true;
+        _behaviourAgentComponent.enabled = true;
+        _navMeshAgentComponent.enabled = true;
+        _animationEventListenerScript.enabled = true;
+        #endregion
 
         return _entity;
     }
