932df2d8dbf945cb1ad354430b5f45d785d84306
Emil Rohlen <emil.rohlen@live.se>
2026-03-12 11:19:46 +0100

    fixed a bug that broke the lore dump scene

--- Assets/Scripts/UI/Narration/DialogueManager.cs
+++ Assets/Scripts/UI/Narration/DialogueManager.cs
@@ -21,8 +21,7 @@ namespace UI.Narration
 
         public static DialogueManager INSTANCE;
         
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        private void Start()
+        private void Awake()
         {
             if (INSTANCE)
             {
