9c504447bca64fad99e7e7accce9761c68b13daf
Emil Rohlén <emil.rohlen@live.se>
2026-03-13 23:53:38 +0100

    gate logic and right branch locked

---
+++ Assets/Scripts/Animation/Gate.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Animation
+{
+    public class Gate : MonoBehaviour
+    {
+        [SerializeField] private Transform _openPosition;
+        [SerializeField] private Transform _closedPosition;
+        [SerializeField] private float _duration = 2f;
+        [SerializeField] private float _shakeIntensity = 0.5f;
+        
+        private bool _isOpen;
+        
+        public void Toggle()
+        {
+            StartCoroutine(_isOpen ? OpenRoutine() : CloseRoutine());
+
+            _isOpen = !_isOpen;
+        }
+        
+        public void Open() => StartCoroutine(OpenRoutine());
+        public void Close() => StartCoroutine(CloseRoutine());
+
+        private IEnumerator OpenRoutine()
+        {
+            float elapsedTime = 0;
+            while (elapsedTime < _duration)
+            {
+                Vector3 shakeOffset = Random.insideUnitSphere * _shakeIntensity;
+                
+                float t = Mathf.Clamp01(elapsedTime / _duration);
+                transform.position = Vector3.Lerp(_closedPosition.position, _openPosition.position, t) + shakeOffset;
+                
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+        }
+        
+        private IEnumerator CloseRoutine()
+        {
+            float elapsedTime = 0;
+            while (elapsedTime < _duration)
+            {
+                Vector3 shakeOffset = Random.insideUnitSphere * _shakeIntensity;
+                
+                float t = Mathf.Clamp01(elapsedTime / _duration);
+                transform.position = Vector3.Lerp(_openPosition.position, _closedPosition.position, t) + shakeOffset;
+                
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+        }
+    }
+}
