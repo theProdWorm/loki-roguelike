32c25b54cc09c49b32a3a2bca7d572b8f34ba8d5
Thelxinoe5 <emiljosef05@gmail.com>
2026-03-20 16:47:10 +0100

    Wolf VFX!!! WAHOOOOO

--- Assets/Scripts/Helpers/Events/AnimationEventListener.cs
+++ Assets/Scripts/Helpers/Events/AnimationEventListener.cs
@@ -11,12 +11,16 @@ namespace Helpers.Events
         public UnityEvent OnSwitch;
         public UnityEvent OnDeath;
         public UnityEvent OnIdle;
-        
+        public UnityEvent OnWolfJump;
+	public UnityEvent OnWolfJumpEnd;
+
         private void InvokeAttackStarted() => OnAttackStarted?.Invoke();
         private void InvokeAttackPerformed() => OnAttackPerformed?.Invoke();
         private void InvokeAttackEnded() => OnAttackEnded?.Invoke();
         private void InvokeSwitch() => OnSwitch?.Invoke();
         private void InvokeDeath() => OnDeath?.Invoke();
         private void InvokeIdle() => OnIdle?.Invoke();
+        private void InvokeOnWolfJump() => OnWolfJump?.Invoke();
+        private void InvokeOnWolfJumpEnd() => OnWolfJumpEnd?.Invoke();
     }
 }
