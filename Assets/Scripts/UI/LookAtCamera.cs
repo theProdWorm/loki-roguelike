c85922148a64027812b5fa247169c0ef98b395ef
b24oliwa <b24oliwa@student.his.se>
2026-03-24 11:17:28 +0100

    fixed glows och lysmask i hub

--- Assets/Scripts/UI/LookAtCamera.cs
+++ Assets/Scripts/UI/LookAtCamera.cs
@@ -4,9 +4,11 @@ namespace UI
 {
     public class LookAtCamera : MonoBehaviour
     {
+        private static Camera mainCamera;
         private void LateUpdate()
         {
-            gameObject.transform.LookAt(Camera.main.transform);
+            if(!mainCamera) mainCamera = Camera.main;
+            gameObject.transform.LookAt(mainCamera?.transform);
         }
     }
 }
