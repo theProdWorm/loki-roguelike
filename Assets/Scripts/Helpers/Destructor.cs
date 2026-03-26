89c388af472a12efac1c39b7dd23bc7924a04593
Emil Rohlén <emil.rohlen@live.se>
2026-03-26 23:06:44 +0100

    component destructor

--- Assets/Scripts/Helpers/Destructor.cs
+++ Assets/Scripts/Helpers/Destructor.cs
@@ -5,5 +5,6 @@ namespace Helpers
     public class Destructor : MonoBehaviour
     {
         public void DestroyObject(GameObject obj) => Destroy(obj);
+        public void DestroyComponent(Component component) => Destroy(component);
     }
 }
